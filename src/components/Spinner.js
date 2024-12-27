930a8dfb0b4aca43003ac15422992993d2a21347
evelyndisa <evelyndisa@icloud.com>
2024-12-27 14:39:40 -0300

    change atribute class

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,7 +4,7 @@ import '../assets/css/Spinner.css'
 export default function Spinner (){
 
     return(
-        <div class="lds-spinner">
+        <div className="lds-spinner">
             <div></div>
             <div></div>
             <div></div>
